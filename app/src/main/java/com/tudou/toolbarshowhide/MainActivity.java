83baa46cf5faffd5d67e61cbe99805036555ccf6
ToDou <yytodou123@gmail.com>
2015-03-11 19:06:23 +0800

    add something

--- app/src/main/java/com/tudou/toolbarshowhide/MainActivity.java
+++ app/src/main/java/com/tudou/toolbarshowhide/MainActivity.java
@@ -1,18 +1,51 @@
 package com.tudou.toolbarshowhide;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageButton;
+
+import com.tudou.toolbarshowhide.adapter.RecyclerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
+
+    private Toolbar mToolbar;
+    private ImageButton mFabButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+        mFabButton = (ImageButton) findViewById(R.id.fabButton);
+        initRecyclerView();
+    }
 
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        setTitle(getString(R.string.app_name));
+        mToolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+    }
 
+    private void initRecyclerView() {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(createItemList());
+        recyclerView.setAdapter(recyclerAdapter);
+    }
+
+    private List<String> createItemList() {
+        List<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            arrayList.add("item" + i);
+        }
+        return arrayList;
     }
 }
---
+++ app/src/main/java/com/tudou/toolbarshowhide/adapter/viewholder/RecyclerHeaderViewHolder.java
@@ -0,0 +1,14 @@
+package com.tudou.toolbarshowhide.adapter.viewholder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by tudou on 15-3-11.
+ */
+public class RecyclerHeaderViewHolder extends RecyclerView.ViewHolder {
+
+    public RecyclerHeaderViewHolder(View itemView) {
+        super(itemView);
+    }
+}
