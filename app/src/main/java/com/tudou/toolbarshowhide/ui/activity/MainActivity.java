0e3f6e6f82fa5468d5c3536fa2273bcdebc7f80a
ToDou <yytodou123@gmail.com>
2015-03-12 15:45:58 +0800

    add the hidescrolllistener

--- app/src/main/java/com/tudou/toolbarshowhide/ui/activity/MainActivity.java
+++ app/src/main/java/com/tudou/toolbarshowhide/ui/activity/MainActivity.java
@@ -1,13 +1,19 @@
-package com.tudou.toolbarshowhide;
+package com.tudou.toolbarshowhide.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
+import com.tudou.toolbarshowhide.R;
 import com.tudou.toolbarshowhide.adapter.RecyclerAdapter;
+import com.tudou.toolbarshowhide.listener.HidingScrollListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +45,29 @@ public class MainActivity extends ActionBarActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         RecyclerAdapter recyclerAdapter = new RecyclerAdapter(createItemList());
         recyclerView.setAdapter(recyclerAdapter);
+        recyclerView.setOnScrollListener(new HidingScrollListener() {
+            @Override
+            public void onHide() {
+                hideViews();
+            }
+
+            @Override
+            public void onShow() {
+                showViews();
+            }
+        });
+    }
+
+    private void hideViews() {
+        mToolbar.animate().translationY(-mToolbar.getHeight()).setInterpolator(new AccelerateInterpolator(2));
+        FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mFabButton.getLayoutParams();
+        int fabBottomMargin = lp.bottomMargin;
+        mFabButton.animate().translationY(mFabButton.getHeight() + fabBottomMargin).setInterpolator(new AccelerateInterpolator(2)).start();
+    }
+
+    private void showViews() {
+        mToolbar.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2));
+        mFabButton.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2)).start();
     }
 
     private List<String> createItemList() {
